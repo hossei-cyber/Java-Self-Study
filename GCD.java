7a76e5ba2e41f34b472aef27e5e87dbe4ad1c5b0
hossei-cyber <hussayaqobzai@gmail.com>
2024-06-11 11:42:19 +0200

    2nd day

---
+++ GCD.java
@@ -0,0 +1,14 @@
+public class GCD {
+    public static void main (String[] args){
+        int a = 5;
+        int b = 15;
+        int ans = 1;
+        int smaller = Math.min(a, b);
+        for(int i = 1; i <= smaller; i++){
+            if(a % i == 0 && b % i == 0){
+                ans = i;
+            }
+        }
+        System.out.println("GCD of " + a + " and " + b + " is " + ans);
+    }
+}
---
+++ LCM.java
@@ -0,0 +1,13 @@
+public class LCM {
+    public static void main (String [] args){
+        int a = 3;
+        int b = 6;
+        int big = Math.max(a, b);
+        for(int i = big; i <= a*b; i += big){
+            if(i % a == 0 && i % b == 0){
+                System.out.println("LCM of " + a + " and " + b + " is " + i);
+                break;
+            }
+        }
+    }
+}
